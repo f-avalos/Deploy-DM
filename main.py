3a70a10fc5195a56478edad354b1666df05bd629
abordones <allmaxbordones@gmail.com>
2025-07-14 10:00:18 -0400

    no sklearn

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
 # import pandas as pd
-from sklearn.base import BaseEstimator
+# from sklearn.base import BaseEstimator
 import os
 import logging
 from datetime import datetime
